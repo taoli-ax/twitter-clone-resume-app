aedbaf162224f15db147fbe20c54f5a46dbc3f08
Tolle <tolle@gmail.com>
2025-01-21 13:53:05 +0800

    add update and delete api and tests

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 
-from comments.api.serializers import CommentSerializer, CommentForCreateSerializer
+from comments.api.permissions import IsObjectOwner
+from comments.api.serializers import CommentSerializer, CommentForCreateSerializer, CommentForUpdateSerializer
 from comments.models import Comment
 
 
@@ -10,9 +11,36 @@ class CommentViewSet(viewsets.GenericViewSet):
     queryset = Comment.objects.all()
 
     def get_permissions(self):
+        """
+        窃以为 两个以上的permission，会一个一个循环遍历，出错就会中止 返回403
+        """
         if self.action == 'create':
             return [permissions.IsAuthenticated()]
+        if self.action in ('update','destroy'):
+            return [permissions.IsAuthenticated(), IsObjectOwner()]
         return [permissions.AllowAny()]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = CommentForUpdateSerializer(
+            instance=instance,
+            data=request.data,
+        )
+        if not serializer.is_valid():
+            return Response({
+                "success":False,
+                "error":serializer.errors,
+                "message":"please check input."
+                 }, status=status.HTTP_400_BAD_REQUEST)
+        # save触发update还是create取决于 instance是否为空
+        comment = serializer.save()
+        return Response(CommentSerializer(comment).data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        # 原生返回204表示数据已不存在，但为了前端好判断，直接改成200
+        return Response({"success":True},status=status.HTTP_200_OK)
 
 
     def create(self, request):
